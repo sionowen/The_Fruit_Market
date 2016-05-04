87b419c36346d6cd6c752c226aa08b12479bf1a4
sionowen <sionowen34@gmail.com>
2016-05-04 17:04:16 -0500

    Did everything

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+$(document).ready( function() {
+	var userCash = "100";
+
+	var apple = new Fruit ("apple", 2, 0, 0);
+	var banana = new Fruit ("banana", 2, 0, 0);
+	//var grape = new Fruit ("grape", 2, 0, 0);
+	var orange = new Fruit ("orange", 2, 0, 0);
+	var pear = new Fruit ("pear", 2, 0, 0);
+
+	var fruitBasket = [apple, banana, orange, pear];
+
+	$('.cash-avail').text(userCash);
+
+	$('.apple').data('fruit', 0);
+	$('.banana').data('fruit', 1);
+	$('.orange').data('fruit', 2);
+	$('.pear').data('fruit', 3);
+
+
+
+$('.container').on('click', 'button', function (){
+var $fl = fruitBasket[$(this).parent().data('fruit')];
+
+$fl.inv +=1;
+console.log('fl', $fl);
+console.log('pear', pear);
+
+$(this).parent().find('.inventory').text();
+});
+
+
+
+
+
+
+
+
+
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+function Fruit (name, price, avgPrice, inv) {
+	this.name = name,
+	this.price = price,
+	this.avgPrice = avgPrice,
+	this.inv = inv
+};
+});
